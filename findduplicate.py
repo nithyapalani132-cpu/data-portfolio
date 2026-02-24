3a6f8b5e60a60d57ece918a822b4f5ca48534438
Nithya.P <nithyapalani132@gmail.com>
2026-02-24 19:11:42 +0530

    "update findduplicate.py"

---
+++ findduplicate.py
@@ -0,0 +1,11 @@
+def find_duplicates(lst):
+    seen = set()
+    duplicates = set()
+    for item in lst:
+        if item in seen:
+            duplicates.add(item)
+        else:
+            seen.add(item)
+    return list(duplicates)
+
+print(find_duplicates([1,2,3,4,2,5,1]))
